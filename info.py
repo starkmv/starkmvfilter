d0379539694a57098cdb843ee23a56d4bbc65198
starkmv <starkmv94@gmail.com>
2025-06-02 10:02:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://therockers868:therockers866@cluster0.rozyzt7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mongo3076:karna001@cluster0.kybz6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "starkmvbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'starkmvcollection')
 
